95f401ffc47f70d86aa1501c651370c36e66cf9e
mashimarokumo <2732946269@qq.com>
2025-01-05 19:42:24 +0800

    可交互物体gizmos

--- Assets/Scripts/Common/InteractiveBase.cs
+++ Assets/Scripts/Common/InteractiveBase.cs
@@ -16,4 +16,11 @@ public abstract class InteractiveBase : MonoBehaviour
     {
        
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.2f);
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+
 }
